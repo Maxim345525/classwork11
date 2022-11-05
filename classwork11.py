48d187f825250f8441654a2de1531f9b8aaa6f02
msxim_m <maximmaksimchyk@gmail.com>
2022-11-05 12:43:45 +0200

    add code

---
+++ classwork11.py
@@ -0,0 +1,7 @@
+def ex_3(arr):
+    nums = [int(i) for i in arr]
+    nums_sum = 0
+    for i in nums:
+        nums_sum += i
+    mean = nums_sum / len(nums)
+    return f'Sum = {nums_sum}, Arithmetical mean = {mean}'
