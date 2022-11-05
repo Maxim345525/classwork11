6b4c532c71bfe54b7bf1d5bb899291d3caf22eb1
msxim_m <maximmaksimchyk@gmail.com>
2022-11-05 11:49:02 +0200

    add code

---
+++ classwork11.py
@@ -0,0 +1,7 @@
+all_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ]
+string = input("type text")
+total_digits = 0
+for s in string:
+    if s in all_digits:
+        total_digits += 1
+print("Totall digits->", total_digits)
